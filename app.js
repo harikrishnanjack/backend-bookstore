09ba62ee7f1ecb18b8b899ac7783bbd60684d1f4
Harikrishnanjack007 <harikrishnan.p@neoito.com>
2021-06-04 18:05:53 +0530

    User authentication

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require('mongoose');
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(`${process.env.MONGO_URI}`, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true
+}, (err) => {
+  if (!err) {
+    console.log('MongoDB Connection Succeeded.')
+  } else {
+    console.log('Error in DB connection: ' + err)
+  }
+});
+
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to server" });
+});
+
+require('./routes/user.route')(app)
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
